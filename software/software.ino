acf093a7de5fb620ab11ed3e35182d9ed9ad23fc
Mauro <exilaus@hotmail.com>
2016-10-26 13:26:46 +0200

    some fake code...
    studing https://github.com/iandouglas96/piPrint/tree/master/Server

--- software/software.ino
+++ software/software.ino
@@ -6,8 +6,57 @@
  * known to work on Wemos D1 board 
  *  
  * both serial 115200 and UDP 9999 accept g-code 
- */  
+ */ 
+ 
+ /*   PIN Esp4motion board */
+ #define xstep (2)
+ #define xdir (0)  //on mcp23017
+ #define xstop (5) //on mcp23017
+ 
+ #define ystep (0)
+ #define ydir (1)  //on mcp23017
+ #define zstop (6) //on mcp23017
+ 
+ #define zstep (4)
+ #define zdir (2)  //on mcp23017
+ #define zstop (7) //on mcp23017
+ 
+ #define estep (16)
+ #define edir (3)  //on mcp23017
+ #define En (4)  //on mcp23017
+ 
+ #define hs1 (12)  //heathead or servo
+ #define hs2 (13)  //heatbed  or servo
+ #define hs3 (15)  //fan      or servo
+ 
+ #define SDA (14)  //to mcp23017
+ #define SLC (5)  //to mcp23017
+ 
+ /* gobal variables */ 
+int tempset = 0 ;
+int state = 0; //0= idle 1=printing 2=pause
 
+
+/*--Exilaus wip code ---*/
+/* note... rpi code use temperature as second tread always on use global parameter for set temperature and check this value for turn turn on/off he(no pid managed).
+ *  need move it on timer and if possibe add pid control.
+ */
+
+ void controlTemp(int t) {
+
+  // trun on t0 line..
+  // read line a0
+  // turn off t0 line
+  // check  i too high turn off hs"t" else turn on 
+
+}
+
+void waitfortemp(int t){
+  
+}
+
+
+/--- Misan code no touched--/
 #include "configuration.h"
 #include "pin.h"
 #include <ESP8266WiFi.h>
@@ -44,6 +93,8 @@ long over;
 float dx,dy;
 
 volatile boolean busy = false;
+
+
 
 void itr1(void) {
   if(flip) {
